c80b8b85500690c33af07310a8eede88c64c419f
Ayishathulmisriya T <74393394+Aisha-1999@users.noreply.github.com>
2022-07-03 21:46:52 +0530

    Add files via upload

---
+++ BigFish/urls.py
@@ -0,0 +1,32 @@
+"""BigFish URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+import myapp1.urls,myapp2.urls
+from django.contrib.staticfiles.urls import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from.import settings
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('myapp1/', include(myapp1.urls)),
+
+    path('myapp2/', include(myapp2.urls))
+
+]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
